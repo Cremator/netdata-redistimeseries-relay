a973370050583a358935d3ff62e043d2d25d336b
Georgi Chompalov <theenlightener@gmail.com>
2023-10-02 12:29:24 +0300

    rueidis

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/tilinna/clock v1.0.2 // indirect
 require (
 	github.com/goccy/go-json v0.10.2
 	github.com/mediocregopher/radix/v4 v4.1.4
+	github.com/redis/rueidis v1.0.19
 )
--- netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
+++ netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
@@ -11,7 +11,9 @@ import (
 	"time"
 
 	"github.com/goccy/go-json"
-	"github.com/mediocregopher/radix/v4"
+	//"github.com/mediocregopher/radix/v4"
+	"github.com/redis/rueidis"
+	//"github.com/redis/rueidis/internal/cmds"
 
 	"context"
 	"net"
@@ -23,6 +25,8 @@ var (
 	redisTimeSeriesHost string
 	redisDelay          time.Duration
 	logConn             string
+
+// Ctx                 = context.Background()
 )
 
 // Options:
@@ -36,8 +40,11 @@ func init() {
 
 func server() {
 	// listen on a port
-	var ctx = context.Background()
-	vanillaClient, err := (radix.PoolConfig{}).New(ctx, "tcp", redisTimeSeriesHost) // or any other client
+	// vanillaClient, err := (radix.PoolConfig{}).New(Ctx, "tcp", redisTimeSeriesHost) // or any other client
+	// if err != nil {
+	// 	log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
+	// }
+	rts, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{redisTimeSeriesHost}, MaxFlushDelay: redisDelay, AlwaysPipelining: true})
 	if err != nil {
 		log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
 	}
@@ -55,27 +62,30 @@ func server() {
 			continue
 		}
 		// handle the connection
-		go handleServerConnection(c, vanillaClient, ctx)
+		go handleServerConnection(c, rts)
 	}
 }
 
-func handleServerConnection(c net.Conn, client radix.Client, ctx context.Context) {
+func handleServerConnection(c net.Conn, client rueidis.Client) {
 	defer c.Close()
 	defer client.Close()
 	reader := bufio.NewScanner(c)
 	var rcv map[string]interface{}
-	rem := c.RemoteAddr().String()
-	p := radix.NewPipeline()
-	delay := time.Now()
+	//rem := c.RemoteAddr().String()
+	//p := radix.NewPipeline()
+	//delay := time.Now()
 	reg, err := regexp.Compile("[^a-zA-Z0-9_./]+")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error while compiling regex. error = %v", err)
 	}
 
 	for reader.Scan() {
 		line := reader.Bytes()
-		json.Unmarshal(line, &rcv)
-		var labels []string = nil
+		err := json.Unmarshal(line, &rcv)
+		if err != nil {
+			log.Fatalf("Error while unmarshaling JSON. error = %v", err)
+		}
+		var labels map[string]string = nil
 		prefix, labels := preProcessAndAddLabel(rcv, "prefix", reg, labels)
 		hostname, labels := preProcessAndAddLabel(rcv, "hostname", reg, labels)
 		_, labels = preProcessAndAddLabel(rcv, "chart_context", reg, labels)
@@ -88,48 +98,60 @@ func handleServerConnection(c net.Conn, client radix.Client, ctx context.Context
 		_, labels = preProcessAndAddLabel(rcv, "units", reg, labels)
 
 		value := rcv["value"].(float64)
-		timestamp := int64(rcv["timestamp"].(float64) * 1000.0)
+		timestamp := rcv["timestamp"].(string) + "000"
 
 		//Metrics are sent to the database server as prefix:hostname:chart_family:chart_name:metric_name.
 		keyName := prefix + ":" + hostname + ":" + chart_family + ":" + chart_name + ":" + metric_name
-		addCmd := radix.FlatCmd(nil, "TS.ADD", keyName, timestamp, value, labels)
-		p.Append(addCmd)
-		t1 := time.Now()
-		l1 := len(p.Properties().Keys)
-		if l1 > 0 && t1.After(delay.Add(redisDelay)) {
-			if err := client.Do(ctx, p); err != nil {
-				log.Fatalf("Error while adding data points. error = %v", err)
-			}
-			showLog(l1, hostname, rem, delay, t1, string(line))
-			p.Reset()
-			delay = time.Now()
+		//keyLabels := rueidis.Incomplete
+		addCmd := client.B().TsAdd().Key(keyName).Timestamp(timestamp).Value(value).Labels()
+		for prefix, label := range labels {
+			addCmd.Labels(prefix, label)
 		}
+		//addCmd.Build()
+		resp := client.Do(context.Background(), addCmd.Build())
+		if err := resp.Error(); err != nil {
+			log.Fatalf("Error while adding data points. error = %v", err)
+		}
+		//addCmd := radix.FlatCmd(nil, "TS.ADD", keyName, timestamp, value, labels)
+		//rtsLabels := make(rueidis.Commands, 0, 10)
+		//addCmd := client.Do(context.Background(), client.B().TsAdd().Key(keyName).Timestamp(string(timestamp)).Value(value).Labels().Labels("prefix", labels["prefix"]).)
+		//p.Append(addCmd)
+		//t1 := time.Now()
+		//l1 := len(p.Properties().Keys)
+		// if l1 > 0 && t1.After(delay.Add(redisDelay)) {
+		// 	if err := client.Do(Ctx, p); err != nil {
+		// 		log.Fatalf("Error while adding data points. error = %v", err)
+		// 	}
+		// 	showLog(l1, hostname, rem, delay, t1, string(line))
+		// 	p.Reset()
+		// 	delay = time.Now()
+		// }
 	}
 }
 
-func preProcessAndAddLabel(rcv map[string]interface{}, key string, reg *regexp.Regexp, labels []string) (value string, labelsOut []string) {
+func preProcessAndAddLabel(rcv map[string]interface{}, key string, reg *regexp.Regexp, labels map[string]string) (value string, labelsOut map[string]string) {
 	labelsOut = labels
 	if rcv[key] != nil {
 		value = reg.ReplaceAllString(rcv[key].(string), "")
 		if len(value) > 0 {
-			if len(labelsOut) == 0 {
-				labelsOut = append(labelsOut, "LABELS")
-			}
-			labelsOut = append(labelsOut, key, value)
+			// if len(labelsOut) == 0 {
+			// 	labelsOut = append(labelsOut, "LABELS")
+			// }
+			labelsOut[key] = value
 		}
 	}
 	return
 }
 
-func showLog(l1 int, host string, rem string, delay time.Time, t1 time.Time, detailed string) {
-	if logConn == "none" {
-		return
-	}
-	log.Printf("Processed %d entries, %d ms since last data connection from %s - %s...\n", l1, t1.Sub(delay).Milliseconds(), host, rem)
-	if logConn == "detailed" {
-		log.Printf("%s\n", detailed)
-	}
-}
+// func showLog(l1 int, host string, rem string, delay time.Time, t1 time.Time, detailed string) {
+// 	if logConn == "none" {
+// 		return
+// 	}
+// 	log.Printf("Processed %d entries, %d ms since last data connection from %s - %s...\n", l1, t1.Sub(delay).Milliseconds(), host, rem)
+// 	if logConn == "detailed" {
+// 		log.Printf("%s\n", detailed)
+// 	}
+// }
 
 func main() {
 	log.Println("Starting netdata-redistimeseries-relay...")
