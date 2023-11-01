c34dd325ec2d82d076620b179fb1bb1a557ce258
Georgi Chompalov <theenlightener@gmail.com>
2023-11-01 13:24:31 +0200

    some changes

--- netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
+++ netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
@@ -31,24 +31,34 @@ type datapoint struct {
 	Name          string  `json:"name"`
 	Value         float64 `json:"value"`
 	Timestamp     int64   `json:"timestamp"`
+	Timestamp_Str string
+	Labels        map[string]string
+	Keyname       string
 }
 
-func (d *datapoint) Labels() map[string]string {
+type rediscmds struct {
+	Commands  rueidis.Commands
+	StartTime time.Time
+	Client    rueidis.Client
+	Server    net.Conn
+	Limit     int
+	Delay     time.Duration
+}
+
+func (d *datapoint) Prepare() *datapoint {
 	reg, err := regexp.Compile("[^a-zA-Z0-9_./]+")
 	if err != nil {
 		log.Fatalf("Error while compiling regex. error = %v", err)
 	}
-	labels := map[string]string{"prefix": reg.ReplaceAllString(d.Prefix, ""), "hostname": reg.ReplaceAllString(d.Hostname, ""), "chart_id": reg.ReplaceAllString(d.Chart_ID, ""), "chart_name": reg.ReplaceAllString(d.Chart_Name, ""), "chart_family": reg.ReplaceAllString(d.Chart_Family, ""), "chart_context": reg.ReplaceAllString(d.Chart_Context, ""), "chart_type": reg.ReplaceAllString(d.Chart_Type, ""), "units": reg.ReplaceAllString(d.Units, ""), "id": reg.ReplaceAllString(d.ID, ""), "name": reg.ReplaceAllString(d.Name, "")}
-	return labels
+	d.Labels = map[string]string{"prefix": reg.ReplaceAllString(d.Prefix, ""), "hostname": reg.ReplaceAllString(d.Hostname, ""), "chart_id": reg.ReplaceAllString(d.Chart_ID, ""), "chart_name": reg.ReplaceAllString(d.Chart_Name, ""), "chart_family": reg.ReplaceAllString(d.Chart_Family, ""), "chart_context": reg.ReplaceAllString(d.Chart_Context, ""), "chart_type": reg.ReplaceAllString(d.Chart_Type, ""), "units": reg.ReplaceAllString(d.Units, ""), "id": reg.ReplaceAllString(d.ID, ""), "name": reg.ReplaceAllString(d.Name, "")}
+	d.Keyname = d.Labels["prefix"] + ":" + d.Labels["hostname"] + ":" + d.Labels["chart_family"] + ":" + d.Labels["chart_name"] + ":" + d.Labels["name"]
+	d.Timestamp_Str = strconv.FormatInt(d.Timestamp*1000, 10)
+	return d
 }
 
 func (d *datapoint) Insert(r rueidis.Client) error {
-	value := d.Value
-	timestamp := strconv.FormatInt(d.Timestamp*1000, 10)
-	labels := d.Labels()
-	keyName := labels["prefix"] + ":" + labels["hostname"] + ":" + labels["chart_family"] + ":" + labels["chart_name"] + ":" + labels["name"]
-	rCmd := r.B().TsAdd().Key(keyName).Timestamp(timestamp).Value(value).Labels()
-	for key, label := range labels {
+	rCmd := r.B().TsAdd().Key(d.Keyname).Timestamp(d.Timestamp_Str).Value(d.Value).Labels()
+	for key, label := range d.Labels {
 		rCmd.Labels(key, label)
 	}
 	resp := r.Do(context.Background(), rCmd.Build())
@@ -56,6 +66,63 @@ func (d *datapoint) Insert(r rueidis.Client) error {
 		return err
 	}
 	return nil
+}
+
+func (r *rediscmds) Connect() *rediscmds {
+	redis, err := rueidis.NewClient(rueidis.ClientOption{
+		InitAddress:   []string{redisTimeSeriesHost},
+		MaxFlushDelay: 50 * time.Microsecond,
+	})
+	if err != nil {
+		log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
+	}
+	r.Client = redis
+	r.InitCmds()
+	return r
+}
+
+func (r *rediscmds) Flush() *rediscmds {
+	for _, resp := range r.Client.DoMulti(context.Background(), r.Commands...) {
+		if err := resp.Error(); err != nil {
+			log.Fatalf("Error while adding data points. error = %v", err)
+		}
+	}
+	log.Printf("Processed %d entries, accumulated over %s\n", r.Limit, time.Since(r.StartTime))
+	return r.InitCmds()
+}
+
+func (r *rediscmds) InitCmds() *rediscmds {
+	if len(r.Commands) == 0 {
+		r.Commands = make(rueidis.Commands, 0, redisBatch)
+		r.Limit = 0
+		r.StartTime = time.Now()
+	} else {
+		log.Fatalf("Error while creating empty commands slice %v. error length = %d", r.Commands, len(r.Commands))
+	}
+	return r
+}
+
+func (r *rediscmds) Log() {
+	if len(r.Commands) == 0 {
+		r.Commands = make(rueidis.Commands, 0, redisBatch)
+		r.Limit = 0
+	} else {
+		log.Fatalf("Error while creating empty commands slice %v. error length = %d", r.Commands, len(r.Commands))
+	}
+}
+
+func (r *rediscmds) Append(d *datapoint) *rediscmds {
+	if r.Limit >= redisBatch || (time.Since(r.StartTime) >= r.Delay && r.Limit > 0) {
+		r.Flush().InitCmds()
+		return r
+	}
+	addCmd := r.Client.B().TsAdd().Key(d.Keyname).Timestamp(d.Timestamp_Str).Value(d.Value).Labels()
+	for key, label := range d.Labels {
+		addCmd.Labels(key, label)
+	}
+	r.Commands = append(r.Commands, addCmd.Build())
+	r.Limit++
+	return r
 }
 
 // Program option vars:
@@ -101,22 +168,22 @@ func init() {
 	flag.StringVar(&listenAddress, "listen-address", LookupEnvOrString("LISTEN_ADDRESS", "127.0.0.1:8080"), "The host:port for listening for JSON inputs")
 	flag.StringVar(&redisTimeSeriesHost, "redistimeseries-host", LookupEnvOrString("REDIS_ADDRESS", "localhost:6379"), "The host:port for Redis connection")
 	flag.DurationVar(&redisDelay, "redis-delay", LookupEnvOrDuration("REDIS_DELAY", time.Millisecond*500), "Delay and accumulate Redis TS.ADDs duration")
-	flag.IntVar(&redisBatch, "redis-batch", LookupEnvOrInt("REDIS_BATCH", 5000), "Redis TS.ADDs max batch size")
+	flag.IntVar(&redisBatch, "redis-batch", LookupEnvOrInt("REDIS_BATCH", 5000), "Redis TS.ADD cmds max batch size")
 	flag.StringVar(&logConn, "connection-log", LookupEnvOrString("CONN_LOG", "standard"), "Show per connection detailed log - none, standard, detail")
 	flag.Parse()
 }
 
-func rtsClient() rueidis.Client {
-	//r, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{redisTimeSeriesHost}, MaxFlushDelay: redisDelay})
-	r, err := rueidis.NewClient(rueidis.ClientOption{
-		InitAddress:   []string{redisTimeSeriesHost},
-		MaxFlushDelay: 50 * time.Microsecond,
-	})
-	if err != nil {
-		log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
-	}
-	return r
-}
+// func rtsClient() rueidis.Client {
+// 	//r, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{redisTimeSeriesHost}, MaxFlushDelay: redisDelay})
+// 	r, err := rueidis.NewClient(rueidis.ClientOption{
+// 		InitAddress:   []string{redisTimeSeriesHost},
+// 		MaxFlushDelay: 50 * time.Microsecond,
+// 	})
+// 	if err != nil {
+// 		log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
+// 	}
+// 	return r
+// }
 
 func server() {
 	//r, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{redisTimeSeriesHost}, MaxFlushDelay: redisDelay})
@@ -124,14 +191,15 @@ func server() {
 	// if err != nil {
 	// 	log.Fatalf("Error while creating new connection to %s. error = %v", redisTimeSeriesHost, err)
 	// }
-	r := rtsClient()
+	r := rediscmds{}
+	r.Connect().InitCmds()
 	s, err := net.Listen("tcp", listenAddress)
 	if err != nil {
 		log.Fatalf("Error while trying to listen to %s. error = %v", listenAddress, err)
 		return
 	}
-	log.Printf("Listening at %s for netdata JSON inputs, and pushing RedisTimeSeries datapoints to %s...\n", listenAddress, redisTimeSeriesHost)
 	log.Printf("Configured redis delay is %s, netdata JSON connection batch size is %d, and logs %s...\n", redisDelay, redisBatch, logConn)
+	log.Printf("Listening at %s for netdata JSON inputs, and pushing RedisTimeSeries datapoints to %s...\n", listenAddress, redisTimeSeriesHost)
 	for {
 		// accept a connection
 		c, err := s.Accept()
@@ -139,29 +207,31 @@ func server() {
 			log.Println(err)
 			continue
 		}
+		r.Server = c
 		// handle the connection
-		go handleServerConnection(c, r)
+		go handleServerConnection(r)
 	}
 }
 
-func handleServerConnection(c net.Conn, r rueidis.Client) {
-	defer c.Close()
-	defer r.Close()
-	c.SetDeadline(time.Now().Add(redisDelay))
+func handleServerConnection(r rediscmds) {
+	defer r.Server.Close()
+	defer r.Client.Close()
+	//c.SetDeadline(time.Now().Add(redisDelay))
 	//tnow := time.Now()
-	reader := bufio.NewScanner(c)
-	reader.Split(bufio.ScanLines)
-	rcv := datapoint{}
+	reader := bufio.NewScanner(r.Server)
+	//reader.Split(bufio.ScanLines)
+
 	//rem := c.RemoteAddr().String()
 	//cmds := make(rueidis.Commands, 0, redisBatch)
 
 	for reader.Scan() {
 		line := reader.Bytes()
+		rcv := datapoint{}
 		err := json.Unmarshal(line, &rcv)
 		if err != nil {
 			log.Fatalf("Error while unmarshaling JSON. error = %v", err)
 		}
-		rcv.Insert(r)
+		r.Append(&rcv)
 		// value := rcv.Value
 		// timestamp := strconv.FormatInt(rcv.Timestamp*1000, 10)
 		// labels := rcv.Labels()
