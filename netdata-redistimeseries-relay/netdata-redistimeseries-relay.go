4545f59e50706db632f36975d1c2de39992dfb5b
Georgi Chompalov <theenlightener@gmail.com>
2023-12-31 12:08:10 +0200

    debug ticker

--- netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
+++ netdata-redistimeseries-relay/netdata-redistimeseries-relay.go
@@ -158,14 +158,14 @@ func ticker(b *atomic.Uint64, r rueidis.Client) {
 	t := time.NewTicker(redisDelay)
 	go func() {
 		for ; ; <-t.C {
-			l := b.Load()
-			b.Store(0)
-			if l >= uint64(redisBatch) {
+			log.Printf("debug netdataredistimeseriesrelay:counter %d...\n", b.Load())
+			if l := b.Load(); l >= uint64(redisBatch) {
 				respi := r.Do(context.Background(), r.B().TsIncrby().Key("netdataredistimeseriesrelay:counter").Value(float64(l)).Build())
 				if err := respi.Error(); err != nil {
 					log.Printf("Error while trying to increase datapoint %d. error = %v\n", b, err)
 				}
 				log.Printf("Increased netdataredistimeseriesrelay:counter with %d...\n", l)
+				b.Store(0)
 			}
 		}
 	}()
